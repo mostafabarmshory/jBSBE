32315a11909389023550dd0a8b50c2d79175b2a5
mostafabarmshory <mostafa.barmshory@dpq.co.ir>
2019-02-26 10:13:12 +0330

    Adding document to the Iso8583 class.

--- src/main/java/org/mashad/jbsbe/annotation/Iso8583.java
+++ src/main/java/org/mashad/jbsbe/annotation/Iso8583.java
@@ -5,12 +5,54 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * This annotation tells that the following POJO represents an ISO Message.
+ * 
+ * It is used with the combination with {@link AutoStan}.
+ * 
+ * In the following example, the POJO is annotated to be ISO message with type
+ * of 0x200:
+ * 
+ * <pre>
+ * <code>
+ *     &#64;Iso8583(type = 0x200)
+ *     &#64;AutoStan
+ *     public class PurchaseRequest {
+ * 	&#64;IsoField(index = 10)
+ * 	public Date date;
+ * 	&#64;IsoField(index = 4)
+ * 	public Long amount;
+ * 	&#64;IsoField(index = 35)
+ * 	public String cardNumber;
+ *     }
+ * </code>
+ * </pre>
+ * 
+ * @see AutoStan
+ */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Iso8583 {
-	int type();
-	String header() default "";
-	boolean binary() default false;
-	
+
+    /**
+     * Defines type of the message
+     * 
+     * @return
+     */
+    int type();
+
+    /**
+     * Gets the header of the message
+     * 
+     * @return
+     */
+    String header() default "";
+
+    /**
+     * Defines the binary encoding mode
+     * 
+     * @return true if the message is encoding in binary format
+     */
+    boolean binary() default false;
 
 }
